20bb5766bca436f53ea99f1f40eaae54c64c9735
pratibha kumari <pratibha.kumari@amdocs.com>
2019-07-10 10:56:04 +0530

    changes done
    .xml.swo

---
+++ src/main/java/com/example/gitDemo/App.java
@@ -0,0 +1,16 @@
+package com.example.gitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+
+    }
+}
